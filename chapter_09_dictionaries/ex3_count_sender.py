4663221ead760c949578b6817c6cab1e900ed061
Riaz-Uddin-Etu <riazuddinetu23@gmail.com>
2025-11-03 15:43:22 +0600

    update code: email to name

--- chapter_09_dictionaries/ex3_count_sender.py
+++ chapter_09_dictionaries/ex3_count_sender.py
@@ -13,6 +13,7 @@ for line in fhand:
     if not line.startswith('From '): continue
     words = line.split()
     email = words[1]  # Retrieve eamil from list words
-
-    counts[email] = counts.get(email, 0) + 1 # Counting email -- Histogram
+    at = email.find('@') # find position of @
+    name = email[:at] # Slcie name 
+    counts[name] = counts.get(name, 0) + 1 # Counting email -- Histogram
 pprint.pprint(counts)
