b0df6335d9c43aa74f5a72fec228b68c75459548
Riaz-Uddin-Etu <riazuddinetu23@gmail.com>
2025-11-17 10:54:02 +0600

    update: urllib to rtrv img - solve running out of memory

--- python-to-access-web-data/chapter-12-network/page152-urllib-retrieve-image.py
+++ python-to-access-web-data/chapter-12-network/page152-urllib-retrieve-image.py
@@ -1,8 +1,18 @@
 # Reading binary files using urllib
 import urllib.request, urllib.parse, urllib.error
 
-img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg').read()
+img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg')
 
 fhand = open('cover3.jpg', 'wb') # write a file in storage
-fhand.write(img) # write binary file (image) in cover3.jpg
+
+size = 0
+while True:
+    data = img.read(100000)
+    if len(data) < 1: break # guardian pattern
+    size = size + len(data)
+    # print(len(data), size)
+    fhand.write(data)
+
+print('Total downloaded: ', size)
+
 fhand.close()
